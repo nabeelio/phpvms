4d7e03855257dc8c23d175c07c4bf1a8612333cd
Nabeel Shahzad <nshahzad@gmail.com>
2017-06-10 22:50:53 -0500

    updated composer files

--- config/app.php
+++ config/app.php
@@ -105,7 +105,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     'aliases' => [
