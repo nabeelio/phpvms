d52f1cec051f54ca6bb14e55f7257eb40f770bc7
Nabeel Shahzad <nshahzad@gmail.com>
2017-06-17 16:41:00 -0500

    new/edit fares page

--- resources/views/admin/fares/fields.blade.php
+++ resources/views/admin/fares/fields.blade.php
@@ -1,4 +1,44 @@
 
+<div class="form-group col-sm-6">
+    {!! Form::label('code', 'Code:') !!}
+    {!! Form::text('code', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('name', 'Name:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('price', 'Price:') !!}
+    {!! Form::text('price', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('cost', 'Cost:') !!}
+    {!! Form::text('cost', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('capacity', 'Capacity:') !!}
+    {!! Form::text('capacity', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('notes', 'Notes:') !!}
+    {!! Form::text('notes', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Active Field -->
+<div class="form-group col-sm-6">
+    <div class="checkbox">
+        <label class="checkbox-inline">
+            {!! Form::hidden('active', false) !!}
+            {!! Form::checkbox('active', '1', true) !!}
+            {!! Form::label('active', 'Active') !!}
+        </label>
+    </div>
+</div>
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
--- resources/views/admin/fares/show_fields.blade.php
+++ resources/views/admin/fares/show_fields.blade.php
@@ -1,9 +1,3 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $fare->id !!}</p>
-</div>
-
 <!-- Code Field -->
 <div class="form-group">
     {!! Form::label('code', 'Code:') !!}
@@ -39,16 +33,3 @@
     {!! Form::label('active', 'Active:') !!}
     <p>{!! $fare->active !!}</p>
 </div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $fare->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $fare->updated_at !!}</p>
-</div>
-
