bbde1ccfd624bd3a42ae54df1dc364fdc3694cb7
Nabeel Shahzad <nshahzad@gmail.com>
2017-06-19 09:47:26 -0500

    update views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,8 +73,16 @@
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
     <div class="top-right links">
-        <a href="{{ url('/login') }}">Login</a>
-        <a href="{{ url('/register') }}">Register</a>
+        @if(!Auth::user())
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
+        @else
+            @if(Entrust::hasRole('admin'))
+                <a href="{{ url('/admin') }}">Admin</a>
+            @endif
+            <a href="{{ url('/dashboard') }}">Dashboard</a>
+            <a href="{{ url('/logout') }}">Logout</a>
+        @endif
     </div>
     @endif
 
@@ -83,13 +91,6 @@
             phpVMS<sup style="">4</sup>
         </div>
 
-        <!--<div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>-->
     </div>
 </div>
 </body>
