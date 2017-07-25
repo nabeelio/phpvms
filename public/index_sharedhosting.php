d8c4dd21da505211e90f13c4e0e486069723450e
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2017-07-25 10:03:00 -0500

    Fix path to phpvms folder

--- public/index_sharedhosting.php
+++ public/index_sharedhosting.php
@@ -26,7 +26,7 @@
 
 require __DIR__.'/../../bootstrap/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../phpvms/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
