6208f67026f0cb354b9cbc33f22bcef601e36ab9
Kyle <kyle.reaume@me.com>
2017-08-12 05:06:54 +0000

    Error Pages added

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default.app')
+@section('content')
+<div class="container registered-page">
+<h3>Unauthorized</h3>
+<p>Well, this is embarrassing, you are not authorized to access or perform this function. Click <a href="{{ url()->previous() }}">here</a> to go back to the home page.</p>
+</div>
+@endsection
