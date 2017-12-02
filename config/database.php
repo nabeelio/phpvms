6b1e52f70b63cb9a8688872b3462a71dd56a3297
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-01 22:31:10 -0600

    mysql in travis

--- config/database.php
+++ config/database.php
@@ -33,6 +33,20 @@ return [
             'prefix' => '',
         ],
 
+        'travis' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'testing' => [
             'driver' => 'sqlite',
             'database' => ':memory:',
