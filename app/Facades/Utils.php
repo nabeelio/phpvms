dc5b8a456944def920214d4c001cfb0de49d6836
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-04 17:39:45 -0600

    lock in more package versions

--- app/Facades/Utils.php
+++ app/Facades/Utils.php
@@ -6,6 +6,12 @@ use \Illuminate\Support\Facades\Facade;
 
 class Utils extends Facade
 {
+
+    protected static function getFacadeAccessor()
+    {
+        return 'utils';
+    }
+
     /**
      * Convert seconds to an array of hours, minutes, seconds
      * @param $seconds
