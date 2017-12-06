c58b8fb7876f0dff0b01445b5d5645caf4ce187f
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-05 21:51:15 -0600

    re-add Sample module

---
+++ modules/Sample/Providers/EventServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Sample\Providers;
+
+use App\Events\TestEvent;
+use Modules\Sample\Listeners\TestEventListener;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     */
+    protected $listen = [
+        TestEvent::class => [TestEventListener::class],
+    ];
+
+    /**
+     * Register any events for your application.
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+}
---
+++ modules/Sample/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('sample::layouts.frontend')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('sample.name') !!}
+    </p>
+@endsection
