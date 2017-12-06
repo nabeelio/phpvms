3b53f884d165f9f7ec82d18d31f89b7fb2a12d94
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-06 11:25:41 -0600

    db cleanup/try phpunit with in-memory tests

--- config/database.php
+++ config/database.php
@@ -39,21 +39,7 @@ return [
             'prefix' => '',
         ],
 
-        'travis' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'testing' => [
+        'memory' => [
             'driver' => 'sqlite',
             'database' => ':memory:',
             'prefix' => '',
