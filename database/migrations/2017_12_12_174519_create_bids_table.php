1da0901ae3c150f5d2a563771cf926239606e50d
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-12 11:52:24 -0600

    Add bids table and bid configuration

---
+++ database/migrations/2017_12_12_174519_create_bids_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_bids', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id', false, true);
+            $table->uuid('flight_id');
+            $table->timestamps();
+
+            $table->index('user_id');
+            $table->index(['user_id', 'flight_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_bids');
+    }
+}
