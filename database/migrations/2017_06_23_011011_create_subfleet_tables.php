695900a0086c5ea4b06daff3ba069fd3fe6c0a7f
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-12 11:57:52 -0600

    Update migrations and add more default settings

--- database/migrations/2017_06_23_011011_create_subfleet_tables.php
+++ database/migrations/2017_06_23_011011_create_subfleet_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSubfleetsTable extends Migration
+class CreateSubfleetTables extends Migration
 {
 
     /**
--- database/migrations/2017_06_28_195426_create_pirep_tables.php
+++ database/migrations/2017_06_28_195426_create_pirep_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePirepsTable extends Migration
+class CreatePirepTables extends Migration
 {
 
     /**
