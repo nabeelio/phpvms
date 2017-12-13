322ea126749e3966ebfe44ae9c38803685112f09
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-13 17:26:16 -0600

    try changing root dir name

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -13,6 +13,6 @@ if [ "$TRAVIS" = "true" ]; then
     rm -rf .git deploy_rsa.enc .idea phpvms.iml .travis .dpl
     find . -type d -name ".git" -delete
 
-    tar -czf $PKG_NAME.tar.gz -C $TRAVIS_BUILD_DIR .
+    tar -czf $PKG_NAME.tar.gz -C $TRAVIS_BUILD_DIR phpvms
     rsync -r --delete-after --quiet $PKG_NAME.tar.gz downloads@phpvms.net:/var/www/downloads/
 fi
