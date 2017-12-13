9d5ab0fafb1b344d0f1d0e5e29142bf28124c115
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-13 17:45:00 -0600

    maybe this will work

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -15,9 +15,7 @@ if [ "$TRAVIS" = "true" ]; then
     find . -type d -name ".git" | xargs rm -rf
 
     # tar and upload
-    mkdir -p /tmp/out/phpvms
-    cp -a $TRAVIS_BUILD_DIR/. /tmp/out/phpvms
-    tar -czf $PKG_NAME.tar.gz -C /tmp/out/
+    tar -czf $PKG_NAME.tar.gz -C $TRAVIS_BUILD_DIR/../
     rsync -r --delete-after --quiet $PKG_NAME.tar.gz downloads@phpvms.net:/var/www/downloads/
     rm -rf /tmp/out
 fi
