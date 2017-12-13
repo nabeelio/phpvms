bcd19580b059438eb5ced065e6c9925e89990ceb
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-13 17:38:12 -0600

    attempt to get the root directory named right

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-if test "$TRAVIS_TAG"; then
-    PKG_NAME=$TRAVIS_TAG
-else
-    PKG_NAME=nightly
-fi
-
-echo $PKG_NAME
-
 if [ "$TRAVIS" = "true" ]; then
+
+    if test "$TRAVIS_TAG"; then
+        PKG_NAME=$TRAVIS_TAG
+    else
+        PKG_NAME=nightly
+    fi
+
+    echo "Writing $PKG_NAME.tar.gz"
 
     # delete all superfluous files
     rm -rf .git deploy_rsa.enc .idea phpvms.iml .travis .dpl
     find . -type d -name ".git" | xargs rm -rf
 
     # tar and upload
-    tar -czf $PKG_NAME.tar.gz -C $TRAVIS_BUILD_DIR phpvms
+    mkdir -p /tmp/out/phpvms
+    cp -R $TRAVIS_BUILD_DIR/ /tmp/out/phpvms
+    tar -czf $PKG_NAME.tar.gz -C /tmp/out/
     rsync -r --delete-after --quiet $PKG_NAME.tar.gz downloads@phpvms.net:/var/www/downloads/
+    rm -rf /tmp/out
 fi
