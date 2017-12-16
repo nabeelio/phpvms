3d43864bbd0f1a9e07393a028735076a05816e72
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-15 19:31:32 -0600

    Make path to phpvms easier to change

--- public/index.php
+++ public/index.php
@@ -10,12 +10,14 @@
  * the 'path.public' path to where it lives.
  */
 
-require __DIR__.'/../bootstrap/autoload.php';
+$path_to_phpvms_folder = __DIR__.'/../';
+
+require $path_to_phpvms_folder.'/bootstrap/autoload.php';
 
 /**
  * @var $app Illuminate\Foundation\Application
  */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $path_to_phpvms_folder.'/bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
