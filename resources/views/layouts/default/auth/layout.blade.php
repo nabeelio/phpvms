89cde530a0087e7a3574367a5759a4d444eaa026
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-24 10:15:55 -0600

    fix page title

--- resources/views/layouts/default/auth/layout.blade.php
+++ resources/views/layouts/default/auth/layout.blade.php
@@ -7,16 +7,12 @@
     <link rel="apple-touch-icon" sizes="76x76" href="/assets/frontend/img/apple-icon.png">
     <link rel="icon" type="image/png" href="/assets/frontend/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Login Page - Now Ui Kit by Creative Tim</title>
+    <title>@yield('title') - {!! config('app.name') !!}</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-    <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-    <!-- CSS Files -->
     <link href="{!! public_asset('/assets/frontend/css/bootstrap.min.css') !!}" rel="stylesheet" />
     <link href="{!! public_asset('/assets/frontend/css/now-ui-kit.css') !!}" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-
     @yield('css')
 </head>
 
