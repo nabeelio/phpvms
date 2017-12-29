6e9c16125de10a2ddda7992a2767409792d57328
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-28 18:13:01 -0600

    remove the .git folder which could be too large

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -18,6 +18,9 @@ if [ "$TRAVIS" = "true" ]; then
     find ./vendor -type d -name ".git" | xargs rm -rf
     rm -rf .idea phpvms.iml .travis .dpl
     rm -rf .phpstorm.meta.php _ide_helper.php
+
+    # remove large sized files
+    rm -rf .git
     rm -rf node_modules
 
     # delete files in vendor that are rather large
