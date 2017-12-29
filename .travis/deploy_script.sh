50da3a9e543b2926fc5014b884e03506b7dcb022
Nabeel Shahzad <nshahzad@gmail.com>
2017-12-29 10:30:49 -0600

    rebuild the web assets in the production mode

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -11,6 +11,9 @@ if [ "$TRAVIS" = "true" ]; then
     TAR_NAME="phpvms-7.0.0-$PKG_NAME.tar.gz"
     echo "Writing $TAR_NAME"
 
+    echo "running build"
+    npm run prod
+
     # delete all superfluous files
     echo "cleaning files"
 
