fe7278c3b7af08fa495fc442c12f12a1dde4d50f
Nabeel Shahzad <nshahzad@gmail.com>
2018-01-04 21:52:55 -0600

    see if var names are replaced

--- .travis/deploy_script.sh
+++ .travis/deploy_script.sh
@@ -52,6 +52,9 @@ if [ "$TRAVIS" = "true" ]; then
     echo "running rsync"
     rsync -ahP --delete-after /tmp/$TAR_NAME downloads@phpvms.net:/var/www/downloads/
 
+    export $TAR_NAME
+    export $VERSION
+
     curl -X POST --data '{"content": "A new build is available at http://phpvms.net/downloads/$TAR_NAME ($VERSION)"}' -H "Content-Type: application/json"  $DISCORD_WEBHOOK_URL
 
 fi
