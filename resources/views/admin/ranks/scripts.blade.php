da19152a1ea56fcc671b2ba074100ab9b6b9a62f
Nabeel Shahzad <nshahzad@gmail.com>
2018-01-04 21:37:46 -0600

    fix broken add rank

--- resources/views/admin/ranks/scripts.blade.php
+++ resources/views/admin/ranks/scripts.blade.php
@@ -3,11 +3,6 @@
 $(document).ready(function () {
     $(".select2").select2();
 
-    $(document).on('submit', 'form.add_rank', function (event) {
-        event.preventDefault();
-        $.pjax.submit(event, '#ranks_table_wrapper', {push: false});
-    });
-
     $(document).on('submit', 'form.pjax_form', function (event) {
         event.preventDefault();
         $.pjax.submit(event, '#rank_subfleet_wrapper', {push: false});
