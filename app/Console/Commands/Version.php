333f866232f5a45a0341362ebd062fcc60cd64aa
Nabeel Shahzad <nshahzad@gmail.com>
2018-01-19 12:08:43 -0500

    include build date in version number #141

--- app/Console/Commands/Version.php
+++ app/Console/Commands/Version.php
@@ -10,6 +10,22 @@ class Version extends BaseCommand
     protected $signature = 'phpvms:version {--write}';
 
     /**
+     * Create the version number that gets written out
+     */
+    protected function createVersionNumber($cfg)
+    {
+        exec($cfg['git']['git-local'], $version);
+        $version = substr($version[0], 0, $cfg['build']['length']);
+
+        # prefix with the date in YYMMDD format
+        $date = date('ymd');
+
+        $version = $date.'-'.$version;
+
+        return $version;
+    }
+
+    /**
      * Run dev related commands
      * @throws \Symfony\Component\Yaml\Exception\ParseException
      */
@@ -19,8 +35,8 @@ class Version extends BaseCommand
             $version_file = config_path('version.yml');
 
             $cfg = Yaml::parse(file_get_contents($version_file));
-            exec($cfg['git']['git-local'], $version);
-            $version = substr($version[0], 0, $cfg['build']['length']);
+
+            $version = $this->createVersionNumber($cfg);
             $cfg['build']['number'] = $version;
 
             file_put_contents($version_file, Yaml::dump($cfg, 4, 2));
