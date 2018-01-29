9f92f452930f8978e7b1d7b5415ea0dcd675bc79
Nabeel Shahzad <nshahzad@gmail.com>
2018-01-29 13:19:03 -0600

    Remove dead code #156

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,19 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
         $this->app->bind('setting', SettingRepository::class);
-
-        # if there's a local.conf.php in the root, then merge that in
-        /*if(file_exists(base_path('config.php'))) {
-            $local_conf = include base_path('config.php');
-
-            foreach($local_conf as $namespace => $override_config) {
-                $config = $this->app['config']->get($namespace, []);
-                $update_config = array_merge_recursive($config, $override_config);
-                $this->app['config']->set($namespace, $update_config);
-            }
-        }*/
     }
 
     /**
