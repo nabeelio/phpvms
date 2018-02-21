1c143f3b235749d69320917c42e0e7a650bb32b4
Nabeel Shahzad <nshahzad@gmail.com>
2018-02-20 21:30:36 -0600

    subfleet table fix and edit in name

--- resources/views/admin/subfleets/table.blade.php
+++ resources/views/admin/subfleets/table.blade.php
@@ -1,16 +1,20 @@
 <div class="content table-responsive table-full-width">
 <table class="table table-hover table-responsive" id="subfleets-table">
     <thead>
-        <th>Airline</th>
         <th>Name</th>
+        <th>Airline</th>
         <th>Type</th>
         <th></th>
     </thead>
     <tbody>
     @foreach($subfleets as $subfleet)
         <tr>
+            <td>
+                <a href="{!! route('admin.subfleets.edit', [$subfleet->id]) !!}">
+                {!! $subfleet->name !!}
+                </a>
+            </td>
             <td>{!! $subfleet->airline->name !!}</td>
-            <td>{!! $subfleet->name !!}</td>
             <td>{!! $subfleet->type !!}</td>
 
             <td class="text-right">
