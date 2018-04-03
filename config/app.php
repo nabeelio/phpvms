4d3fb5b8738387fde8eedfa65a3c0ffc381d697e
Nabeel Shahzad <nshahzad@gmail.com>
2018-04-03 18:15:10 -0500

    removed/update composer dependencies

--- config/app.php
+++ config/app.php
@@ -79,7 +79,6 @@ return [
         Igaster\LaravelTheme\themeServiceProvider::class,
         Nwidart\Modules\LaravelModulesServiceProvider::class,
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
-        Orchestra\Parser\XmlServiceProvider::class,
 
         /*
          * Application Service Providers...
