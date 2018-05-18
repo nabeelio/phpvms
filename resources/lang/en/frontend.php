33ef3dd25201572706f5977809b41af91f91e72b
Nabeel Shahzad <nshahzad@gmail.com>
2018-05-18 14:56:37 -0500

    Move widgets into separate file

--- resources/lang/en/frontend.php
+++ resources/lang/en/frontend.php
@@ -105,35 +105,4 @@ return [
     'users' => [
         'location' => 'Location',
     ],
-
-    'widgets' => [
-        'latestnews' => [
-            'news' => 'News',
-            'nonewsfound' => 'No News Found',
-        ],
-
-        'livemap' => [
-            'groundspeed' => 'Ground Speed',
-            'altitude' => 'Altitude',
-            'heading' => 'Heading',
-            'noflights' => 'There are no flights.',
-            'gs' => 'GS',
-        ],
-
-        'weather' => [
-            'metarnotretrieved' => 'METAR/TAF data could not be retrieved',
-            'conditions' => 'Conditions',
-            'visibility' => 'visibility',
-            'humidity' => 'humidity',
-            'dewpoint' => 'dew point',
-            'barometer' => 'Barometer',
-            'clouds' => 'Clouds',
-            'wind' => 'Wind',
-            'remarks' => 'Remarks',
-            'guststo' => 'gusts to',
-            'updated' => 'Updated',
-            'hrago' => 'hr ago|hrs ago',
-            'minago' => 'min ago|mins ago',
-        ],
-    ],
 ];
---
+++ resources/lang/en/widgets.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ */
+
+return [
+    'latestnews' => [
+        'news'        => 'News',
+        'nonewsfound' => 'No News Found',
+    ],
+
+    'livemap' => [
+        'groundspeed' => 'Ground Speed',
+        'altitude'    => 'Altitude',
+        'heading'     => 'Heading',
+        'noflights'   => 'There are no flights.',
+        'gs'          => 'GS',
+    ],
+
+    'weather' => [
+        'metarnotretrieved' => 'METAR/TAF data could not be retrieved',
+        'conditions'        => 'Conditions',
+        'visibility'        => 'visibility',
+        'humidity'          => 'humidity',
+        'dewpoint'          => 'dew point',
+        'barometer'         => 'Barometer',
+        'clouds'            => 'Clouds',
+        'wind'              => 'Wind',
+        'remarks'           => 'Remarks',
+        'guststo'           => 'gusts to',
+        'updated'           => 'Updated',
+        'hrago'             => 'hr ago|hrs ago',
+        'minago'            => 'min ago|mins ago',
+    ],
+];
