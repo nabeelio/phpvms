81c148fc4d45d33e254f8e435f305e2063a07a43
lordwilbur <luca_baldi@hotmail.com>
2018-05-18 16:02:49 +0200

    More translations

--- resources/views/layouts/default/errors/503.blade.php
+++ resources/views/layouts/default/errors/503.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>@lang('frontend.errors.503message')</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">@lang('frontend.errors.503message')</div>
             </div>
         </div>
     </body>
