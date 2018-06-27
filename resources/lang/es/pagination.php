8b368a3808e3a30c8a5363fefbae7b34eadb4eb1
arv187 <sparkticus20@hotmail.com>
2018-06-28 01:08:56 +0200

    upload spanish translation to es-translations branch

---
+++ resources/lang/es/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Paginación de líneas de lenguaje
+    |--------------------------------------------------------------------------
+    |
+    | La biblioteca del paginador utiliza las siguientes líneas de idioma para construir
+    | los enlaces simples de paginación. Usted es libre de cambiarlos a cualquier cosa
+    | si desea personalizar sus vistas para que coincida mejor con su aplicación.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next'     => 'Siguiente &raquo;',
+
+];
