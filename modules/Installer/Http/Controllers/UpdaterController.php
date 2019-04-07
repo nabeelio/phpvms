29a578ea6a8242ddeda25a11faa11c6b854fb222
Nabeel Shahzad <nshahzad@gmail.com>
2019-04-07 18:53:24 -0500

    Move the MigrationService to the main package

--- modules/Installer/Http/Controllers/UpdaterController.php
+++ modules/Installer/Http/Controllers/UpdaterController.php
@@ -3,9 +3,10 @@
 namespace Modules\Installer\Http\Controllers;
 
 use App\Interfaces\Controller;
+use App\Services\Installer\MigrationService;
 use Illuminate\Http\Request;
 use Log;
-use Modules\Installer\Services\MigrationService;
+
 
 /**
  * Class UpdaterController
@@ -36,6 +37,10 @@ class UpdaterController extends Controller
     /**
      * Step 1. Check if there's an update available. Check if there
      * are any unrun migrations
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function step1(Request $request)
     {
@@ -49,22 +54,23 @@ class UpdaterController extends Controller
 
     /**
      * Step 2 Run all of the migrations
+     *
      * @param Request $request
+     *
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function run_migrations(Request $request)
     {
         Log::info('Update: run_migrations', $request->post());
 
-        // Resync all of the settings
-        $this->migrationSvc->updateAllSettings();
-
         $migrations = $this->migrationSvc->migrationsAvailable();
         if(\count($migrations) === 0) {
+            $this->migrationSvc->syncAllSeeds();
             return view('installer::update/steps/step3-update-complete');
         }
 
         $output = $this->migrationSvc->runAllMigrations();
+        $this->migrationSvc->syncAllSeeds();
 
         return view('installer::update/steps/step2-migrations-done', [
             'console_output' => $output,
@@ -73,7 +79,9 @@ class UpdaterController extends Controller
 
     /**
      * Final step
+     *
      * @param Request $request
+     *
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function complete(Request $request)
