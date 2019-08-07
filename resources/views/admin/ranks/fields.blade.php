4cadad5df867f7e51bc0e423cf38a1dade569d11
Nabeel Shahzad <nshahzad@gmail.com>
2019-08-07 12:24:35 -0400

    Tooltips for ranks

--- resources/views/admin/ranks/fields.blade.php
+++ resources/views/admin/ranks/fields.blade.php
@@ -50,35 +50,50 @@
             <div class="form-container-body">
                 <div class="row">
                     <!-- Auto Approve Acars Field -->
-                    <div class="form-group col-sm-4 text-center">
+                    <div class="form-group col-sm-4">
                         <div class="checkbox">
                             <label class="checkbox-inline">
                                 {{ Form::hidden('auto_approve_acars', false) }}
                                 {{ Form::checkbox('auto_approve_acars') }}
                                 {{ Form::label('auto_approve_acars', 'Auto Approve ACARS PIREPs') }}
                             </label>
+                            <div style="margin-left: 10px">
+                            @component('admin.components.info')
+                                PIREPS submitted through ACARS are automatically accepted
+                            @endcomponent
+                            </div>
                         </div>
                     </div>
 
                     <!-- Auto Approve Manual Field -->
-                    <div class="form-group col-sm-4 text-center">
+                    <div class="form-group col-sm-4">
                         <div class="checkbox">
                             <label class="checkbox-inline">
                                 {{ Form::hidden('auto_approve_manual', false) }}
                                 {{ Form::checkbox('auto_approve_manual') }}
                                 {{ Form::label('auto_approve_manual', 'Auto Approve Manual PIREPs') }}
                             </label>
+                            <div style="margin-left: 10px">
+                            @component('admin.components.info')
+                                PIREPS submitted manually are automatically accepted
+                            @endcomponent
+                            </div>
                         </div>
                     </div>
 
                     <!-- Auto Promote Field -->
-                    <div class="form-group col-sm-4 text-center">
+                    <div class="form-group col-sm-4">
                         <div class="checkbox">
                             <label class="checkbox-inline">
                                 {{ Form::hidden('auto_promote', false) }}
                                 {{ Form::checkbox('auto_promote') }}
                                 {{ Form::label('auto_promote', 'Auto Promote') }}
                             </label>
+                            <div style="margin-left: 10px">
+                            @component('admin.components.info')
+                                When a pilot reaches these hours, they'll be upgraded to this rank
+                            @endcomponent
+                            </div>
                         </div>
                     </div>
                 </div>
