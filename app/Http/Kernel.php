94ba5d86803370f9eb1c8435323b5a44fc00bc60
Nabeel Shahzad <nshahzad@gmail.com>
2019-08-22 12:45:15 -0400

    Disable the requests throttler

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends HttpKernel
 
     protected $middlewareGroups = [
         'api' => [
-            'throttle:60,1',
+            // 'throttle:60,1',
             'bindings',
             'json',
         ],
