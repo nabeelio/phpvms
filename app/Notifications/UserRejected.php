4cfad2295d7309c5a9a18c42304a4c9853a35ee4
Nabeel Shahzad <nshahzad@gmail.com>
2019-08-30 11:50:45 -0400

    Convert Mailer to Notification; add pirep notifications

---
+++ app/Notifications/UserRejected.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserRejected extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $user;
+
+    /**
+     * @param \App\Models\User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->from(config('mail.from.address', 'no-reply@phpvms.net'))
+            ->subject('Your registration has been denied')
+            ->markdown('mail.user.rejected', ['user' => $this->user]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user_id' => $this->user->id,
+        ];
+    }
+}
