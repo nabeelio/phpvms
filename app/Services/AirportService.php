1511a74403f4ff2dd75fc66a054c133b72774668
Nabeel Shahzad <nshahzad@gmail.com>
2019-09-05 16:29:25 -0400

    Remove missing/unused import

--- app/Services/AirportService.php
+++ app/Services/AirportService.php
@@ -14,7 +14,6 @@ use League\Geotools\Coordinate\Coordinate;
 use League\Geotools\Geotools;
 use PhpUnitsOfMeasure\Exception\NonNumericValue;
 use PhpUnitsOfMeasure\Exception\NonStringUnitName;
-use VaCentral\Airport;
 
 /**
  * Class AnalyticsService
@@ -61,7 +60,7 @@ class AirportService extends Service
      *
      * @param string $icao ICAO
      *
-     * @return Airport|array
+     * @return mixed
      */
     public function lookupAirport($icao)
     {
