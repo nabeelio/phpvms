41a5c69786b3b558d480f62c6e572ae1a06e3dfa
Nabeel Shahzad <nshahzad@gmail.com>
2019-09-16 12:48:31 -0400

    Move Command contract to correct folder

--- app/Console/Commands/TestApi.php
+++ app/Console/Commands/TestApi.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Console\Command;
+use App\Contracts\Command;
 use GuzzleHttp\Client;
 
 class TestApi extends Command
--- app/Console/Commands/YamlImport.php
+++ app/Console/Commands/YamlImport.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Console\Command;
+use App\Contracts\Command;
 use App\Services\DatabaseService;
 
 /**
