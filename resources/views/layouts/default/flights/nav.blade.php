1725ea5a9e0de8f0044eaba1d1193242584eb469
Benjamín Buske <info@buske-it.com>
2019-10-28 13:21:21 -0400

    Some minor CSS fixes as discussed on Discord (#420)
    * Minor CSS Changes
    
    Minor CSS changes to the original. 
    
    #1: Added button to show own bids. 
    #2: Added padding to Search buttons and fields  
    #3: Fixed search fields leaving the search box
    
    * Minor CSS Changes to Search Fields
    
    Search Fields have been changed from round to square for better look.

--- resources/views/layouts/default/flights/nav.blade.php
+++ resources/views/layouts/default/flights/nav.blade.php
@@ -1,6 +1,26 @@
 <div class="card">
-    <div class="card-block" style="min-height: 0px">
-        <div class="form-group text-right">
+    <div class="card-block" style="min-height: 0px; display: flex; justify-content: center; align-items: center;">
+        <style>
+
+            .my-bids {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 50%;
+                margin: 15px;
+                padding: 10px;
+                border-radius: .1875rem;
+                background-color: #fa7a50;
+            }
+
+            .my-bids a {
+                color: #fff;
+                text-decoration: none;
+                text-align: center;
+            }
+        </style>
+
+        <div class="form-group text-right btn-primary my-bids">
             <a href="{{ route('frontend.flights.bids') }}">{{ trans_choice('flights.mybid', 2) }}</a>
         </div>
     </div>
--- resources/views/layouts/default/flights/search.blade.php
+++ resources/views/layouts/default/flights/search.blade.php
@@ -7,22 +7,28 @@
                     'method' => 'GET',
                     'class'=>'form-inline'
             ]) }}
-            <div>
+            <style> 
+            .form-group .form-control {
+             width: 250px;
+             border-radius: 5px;
+            }
+            </style>
+            <div style="margin-left: 5px;">
                 <p>@lang('flights.flightnumber')</p>
                 {{ Form::text('flight_number', null, ['class' => 'form-control']) }}
             </div>
 
-            <div style="margin-top: 10px;">
+            <div style="margin-top: 10px; margin-left: 5px;">
                 <p>@lang('airports.departure')</p>
                 {{ Form::select('dep_icao', $airports, null , ['class' => 'form-control']) }}
             </div>
 
-            <div style="margin-top: 10px;">
+            <div style="margin-top: 10px; margin-left: 5px;">
                 <p>@lang('airports.arrival')</p>
                 {{ Form::select('arr_icao', $airports, null , ['class' => 'form-control']) }}
             </div>
 
-            <div class="clear" style="margin-top: 10px;">
+            <div class="clear" style="margin-top: 10px; margin-left: 5px;">
                 {{ Form::submit(__('common.find'), ['class' => 'btn btn-primary']) }}&nbsp;
                 <a href="{{ route('frontend.flights.index') }}">@lang('common.reset')</a>
             </div>
