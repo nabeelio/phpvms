3e8a1f32c77159e5b89f7d1826d2011fa79d3dcd
Nabeel Shahzad <nshahzad@gmail.com>
2019-10-29 08:30:23 -0400

    Clickable flight IDs #419

--- resources/views/layouts/default/widgets/latest_pireps.blade.php
+++ resources/views/layouts/default/widgets/latest_pireps.blade.php
@@ -5,8 +5,9 @@
                 <span class="title">{{ $p->airline->code }}</span>
             </td>
             <td>
-                {{ $p->dpt_airport_id }}-
-                {{ $p->arr_airport_id }}&nbsp;
+              <a href="{{route('frontend.airports.show', [$p->dpt_airport_id])}}">{{$p->dpt_airport_id}}</a>
+                &nbsp;-&nbsp;
+              <a href="{{route('frontend.airports.show', [$p->arr_airport_id])}}">{{$p->arr_airport_id}}</a>&nbsp;
                 @if($p->aircraft)
                     {{ $p->aircraft->name }}
                 @endif
