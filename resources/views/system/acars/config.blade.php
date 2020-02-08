62b5a98fca75c9ff0fea7ea78459c4c502a8672c
Nabeel Shahzad <nshahzad@gmail.com>
2020-02-07 21:03:21 -0500

    Update xml config file template

--- resources/views/system/acars/config.blade.php
+++ resources/views/system/acars/config.blade.php
@@ -1,4 +1,5 @@
-<AcarsConfiguration xmlns="http://schemas.phpvms.net/acars/config">
+<?xml version = "1.0" encoding = "utf-8"?>
+<AcarsConfiguration xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.phpvms.net/acars/config">
   <AirlineUrl>{{ config('app.url') }}</AirlineUrl>
   <ApiKey>{{ $user->api_key }}</ApiKey>
 </AcarsConfiguration>
