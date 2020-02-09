88b3d9f5313038cb5efa5192a394be95dd4f2768
Jorge Cabal <sininchi@gmail.com>
2020-02-09 23:01:21 +0100

    Spanish files translation

--- resources/lang/es/flights.php
+++ resources/lang/es/flights.php
@@ -21,7 +21,7 @@ return [
     'departuretime'    => 'Hora de salida',
     'arrivaltime'      => 'Hora de llegada',
     'type'             => [
-        'pass_scheduled'    => 'Pasagero - Programado',
+        'pass_scheduled'    => 'Pasajero - Programado',
         'cargo_scheduled'   => 'Carga - Programado',
         'charter_pass_only' => 'Charter - Solo pasajeros',
         'addtl_cargo_mail'  => 'Carga/correo adicional',
