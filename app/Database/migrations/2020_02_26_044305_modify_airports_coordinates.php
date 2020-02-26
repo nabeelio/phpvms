66047ca36acf9ca52b664bb2962b9a677b4d7363
Nabeel Shahzad <nshahzad@gmail.com>
2020-02-26 16:56:06 -0500

    Remove 4 char restriction from ICAO; use decimal type for lat/lon #590

---
+++ app/Database/migrations/2020_02_26_044305_modify_airports_coordinates.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Turn the airport coordinates and other lat/lon coords into decimal type
+ */
+class ModifyAirportsCoordinates extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('acars', function ($table) {
+            $table->decimal('lat', 10, 5)->change()->default(0.0)->nullable();
+            $table->decimal('lon', 11, 5)->change()->default(0.0)->nullable();
+        });
+
+        Schema::table('airports', function ($table) {
+            $table->decimal('lat', 10, 5)->change()->default(0.0)->nullable();
+            $table->decimal('lon', 11, 5)->change()->default(0.0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
