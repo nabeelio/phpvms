8f5e4ee35597c88c84b5f693dbf31a92fce73dc3
Nabeel Shahzad <nshahzad@gmail.com>
2020-02-28 15:57:51 -0500

    Formatting

--- app/Console/Commands/ClearCaches.php
+++ app/Console/Commands/ClearCaches.php
@@ -11,7 +11,7 @@ class ClearCaches extends Command
     protected $description = 'Clear all caches';
 
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
     public function handle()
     {
