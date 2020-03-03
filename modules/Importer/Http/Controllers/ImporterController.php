86d1e1d764af4986824ca933a1a2b7d532da3147
Nabeel Shahzad <nshahzad@gmail.com>
2020-03-02 20:37:54 -0500

    Remove extra logging

--- modules/Importer/Http/Controllers/ImporterController.php
+++ modules/Importer/Http/Controllers/ImporterController.php
@@ -33,12 +33,6 @@ class ImporterController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->isSecure()) {
-            Log::info('request is secure');
-        } else {
-            Log::info('request is not secure');
-        }
-
         return view('importer::step1-configure');
     }
 
