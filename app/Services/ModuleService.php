24eb73eaa1c4cf23e4d9db24ac21b1da04765099
Yash <yash.govekar55@gmail.com>
2020-10-09 18:52:03 +0530

    Updated ModuleService

--- app/Services/ModuleService.php
+++ app/Services/ModuleService.php
@@ -206,7 +206,7 @@ class ModuleService extends Service
         $this->addModule($module);
 
         Artisan::call('config:cache');
-        Artisan::call('module:migrate '.$module);
+        Artisan::call('module:migrate  '.$module);
         return true;
     }
 
