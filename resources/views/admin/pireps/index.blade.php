1598a359d59ce98f2f0ccef8bfc111ca4a2d3018
B.Fatih KOZ <fatih.koz@gmail.com>
2020-12-06 03:46:40 +0300

    Fix and Update for Admin Pirep Index
    Fixed the page width problem and search error , added the ability to also search for Rejected and Accepted pireps.

--- resources/views/admin/pireps/index.blade.php
+++ resources/views/admin/pireps/index.blade.php
@@ -2,16 +2,18 @@
 
 @section('title', 'Pilot Reports')
 @section('actions')
-  <li><a href="{{ route('admin.pirepfields.index') }}"><i class="ti-menu-alt"></i>PIREP Fields</a></li>
-  <li><a href="{{ route('admin.pireps.index') }}?search=status:{{ \App\Models\Enums\PirepState::PENDING }}"><i class="ti-plus"></i>Pending</a></li>
-  <li><a href="{{ route('admin.pireps.index') }}"><i class="ti-plus"></i>View All</a></li>
+	<li><a href="{{ route('admin.pirepfields.index') }}"><i class="ti-menu-alt"></i>PIREP Fields</a></li>
+	<li><a href="{{ route('admin.pireps.index') }}?search=state:{{ \App\Models\Enums\PirepState::PENDING }}"><i class="ti-plus"></i>Pending</a></li>
+	<li><a href="{{ route('admin.pireps.index') }}?search=state:{{ \App\Models\Enums\PirepState::REJECTED }}"><i class="ti-plus"></i>Rejected</a></li>
+	<li><a href="{{ route('admin.pireps.index') }}?search=state:{{ \App\Models\Enums\PirepState::ACCEPTED }}"><i class="ti-plus"></i>Accepted</a></li>
+	<li><a href="{{ route('admin.pireps.index') }}"><i class="ti-plus"></i>View All</a></li>
 @endsection
 @section('content')
-  @include('admin.pireps.table')
-  <div class="row">
-    <div class="col-12 text-center">
-      {{ $pireps->links('admin.pagination.default') }}
-    </div>
-  </div>
+<div class="row" style="width: 100%">
+	@include('admin.pireps.table')
+</div>
+<div class="row text-center" style="width: 100%">
+	{{ $pireps->links('admin.pagination.default') }}
+</div>
 @endsection
 @include('admin.pireps.scripts')
