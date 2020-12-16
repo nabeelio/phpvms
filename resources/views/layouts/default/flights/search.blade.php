b1fe11ae08f6f06286e8329c95e55743556e3bba
B.Fatih KOZ <fatih.koz@gmail.com>
2020-12-16 17:21:38 +0300

    Update search.blade.php

--- resources/views/layouts/default/flights/search.blade.php
+++ resources/views/layouts/default/flights/search.blade.php
@@ -1,42 +1,44 @@
 <h3 class="description">@lang('flights.search')</h3>
 <div class="card border-blue-bottom">
-  <div class="card-body ml-1 mr-1" style="min-height: 0px; display: flex; justify-content: center; align-items: center;">
-    <div class="form-group search-form">
-      {{ Form::open([
-              'route' => 'frontend.flights.search',
-              'method' => 'GET',
-              'class'=>'form-inline'
-      ]) }}
-      <div class="mt-1">
-        <p>@lang('common.airline')</p>
-        {{ Form::select('airline_id', $airlines, null , ['class' => 'form-control select2']) }}
-      </div>
+	<div class="card-body ml-1 mr-1" style="min-height: 0px; display: flex; justify-content: center; align-items: center;">
+		<div class="form-group search-form">
+			{{ Form::open([
+				'route' => 'frontend.flights.search',
+				'method' => 'GET',
+				'class'=>'form-inline'
+			]) }}
+			<div class="mt-1">
+				<p>@lang('common.airline')</p>
+				@php asort($airlines); @endphp
+				{{ Form::select('airline_id', $airlines, null , ['class' => 'form-control select2']) }}
+			</div>
       
-      <div>
-        <p>@lang('flights.flightnumber')</p>
-        {{ Form::text('flight_number', null, ['class' => 'form-control']) }}
-      </div>
+			<div class="mt-1">
+				<p>@lang('flights.flightnumber')</p>
+				{{ Form::text('flight_number', null, ['class' => 'form-control']) }}
+			</div>
 
-      <div class="mt-1">
-        <p>@lang('airports.departure')</p>
-        {{ Form::select('dep_icao', $airports, null , ['class' => 'form-control select2']) }}
-      </div>
+			<div class="mt-1">
+				<p>@lang('airports.departure')</p>
+				{{ Form::select('dep_icao', $airports, null , ['class' => 'form-control select2']) }}
+			</div>
 
-      <div class="mt-1">
-        <p>@lang('airports.arrival')</p>
-        {{ Form::select('arr_icao', $airports, null , ['class' => 'form-control select2']) }}
-      </div>
+			<div class="mt-1">
+				<p>@lang('airports.arrival')</p>
+				{{ Form::select('arr_icao', $airports, null , ['class' => 'form-control select2']) }}
+			</div>
 
-      <div class="mt-1">
-        <p>@lang('common.subfleet')</p>
-        {{ Form::select('subfleet_id', $subfleets, null , ['class' => 'form-control select2']) }}
-      </div>
+			<div class="mt-1">
+				<p>@lang('common.subfleet')</p>
+				@php asort($subfleets); @endphp
+				{{ Form::select('subfleet_id', $subfleets, null , ['class' => 'form-control select2']) }}
+			</div>
 
-      <div class="clear mt-1" style="margin-top: 10px;">
-        {{ Form::submit(__('common.find'), ['class' => 'btn btn-outline-primary']) }}&nbsp;
-        <a href="{{ route('frontend.flights.index') }}">@lang('common.reset')</a>
-      </div>
-      {{ Form::close() }}
-    </div>
-  </div>
+			<div class="clear mt-1" style="margin-top: 10px;">
+				{{ Form::submit(__('common.find'), ['class' => 'btn btn-outline-primary']) }}&nbsp;
+				<a href="{{ route('frontend.flights.index') }}">@lang('common.reset')</a>
+			</div>
+			{{ Form::close() }}
+		</div>
+	</div>
 </div>
