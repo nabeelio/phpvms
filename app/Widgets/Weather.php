202986fd16ad260ac862633a0ef52e401474e2ef
B.Fatih KOZ <fatih.koz@gmail.com>
2020-12-20 10:05:51 +0300

    Update Weather.php
    Used updated Metar\AviationWeather service to improve the widget ability and provide raw TAF data for the view

--- app/Widgets/Weather.php
+++ app/Widgets/Weather.php
@@ -4,15 +4,14 @@ namespace App\Widgets;
 
 use App\Contracts\Widget;
 use App\Services\AirportService;
+use App\Services\Metar\AviationWeather;
 
 /**
  * This is a widget for the 3rd party CheckWX service
  */
 class Weather extends Widget
 {
-    protected $config = [
-        'icao' => null,
-    ];
+    protected $config = ['icao' => null,];
 
     /**
      * Attempt to get the data from the CheckWX API
@@ -20,12 +19,16 @@ class Weather extends Widget
     public function run()
     {
         /** @var \App\Services\AirportService $airportSvc */
-        $airportSvc = app(AirportService::class);
+        $addsSvc = app(AviationWeather::class);
+        $airportSvc = app(AirportService::class);     
         $metar = $airportSvc->getMetar($this->config['icao']);
+        $taf = $addsSvc->taf($this->config['icao']);
 
         return view('widgets.weather', [
             'config'    => $this->config,
+            'icao'      => $this->config['icao'],
             'metar'     => $metar,
+            'taf'       => $taf,
             'unit_alt'  => setting('units.altitude'),
             'unit_dist' => setting('units.distance'),
             'unit_temp' => setting('units.temperature'),
