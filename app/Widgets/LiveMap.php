f9903b6b929e672c81f4b8b7a020e4d83e1b92c9
B.Fatih KOZ <74361521+FatihKoz@users.noreply.github.com>
2021-01-12 16:05:06 +0300

    Update LiveMap.php

--- app/Widgets/LiveMap.php
+++ app/Widgets/LiveMap.php
@@ -14,7 +14,7 @@ class LiveMap extends Widget
     protected $config = [
         'height' => '800px',
         'width'  => '100%',
-        'table'  => 1,
+        'table'  => true,
     ];
 
     /**
