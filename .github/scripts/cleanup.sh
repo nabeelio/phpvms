45af1117a955418e5adf8ac13607c399756b7476
Nabeel Shahzad <nshahzad@gmail.com>
2021-01-16 14:39:40 -0500

    gh actions update

---
+++ .github/scripts/cleanup.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+
+echo "Cleaning files"
+
+rm -rf vendor
+composer install --no-dev --prefer-dist --no-interaction --verbose
+
+# Leftover individual files to delete
+declare -a remove_files=(
+  .git
+  .github
+  .sass-cache
+  .idea
+  .travis
+  docker
+  _ide_helper.php
+  .dockerignore
+  .dpl
+  .editorconfig
+  .eslintignore
+  .eslintrc
+  .php_cs
+  .php_cs.cache
+  .phpstorm.meta.php
+  .styleci.yml
+  .phpunit.result.cache
+  env.php
+  intellij_style.xml
+  config.php
+  docker-compose.yml
+  Makefile
+  phpcs.xml
+  phpunit.xml
+  phpvms.iml
+  Procfile
+  phpstan.neon
+  node_modules
+  composer.phar
+  vendor/willdurand/geocoder/tests
+)
+
+for file in "${remove_files[@]}"; do
+  rm -rf $file
+done
+
+find ./vendor -type d -name ".git" -print0 | xargs rm -rf
+find . -type d -name "sass-cache" -print0 | xargs rm -rf
+
+# clear any app specific stuff that might have been loaded in
+find bootstrap/cache -mindepth 1 -maxdepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/app -mindepth 1 -maxdepth 1 -not -name '.gitignore' -not -name public -not -name import -print0 -exec rm -rf {} +
+find storage/app/public -mindepth 1 -maxdepth 1 -not -name '.gitignore' -not -name avatars -not -name uploads -print0 -exec rm -rf {} +
+find storage/app/public/avatars -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/app/public/uploads -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/debugbar -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/docker -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/framework/cache -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/framework/sessions -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/framework/views -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+find storage/logs -mindepth 1 -not -name '.gitignore' -print0 -exec rm -rf {} +
+
+mkdir -p storage/app/public/avatars
+mkdir -p storage/app/public/uploads
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+
+# Regenerate the autoloader and classes
+composer dump-autoload
