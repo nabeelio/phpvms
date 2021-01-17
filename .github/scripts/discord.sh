a48edaa62118d6da891b506c8032511db5a1681b
Nabeel Shahzad <nshahzad@gmail.com>
2021-01-16 19:39:11 -0500

    gh actions update

--- .github/scripts/discord.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST \
-  --data "{\"content\": \"A new build is available at http://downloads.phpvms.net/$TAR_NAME (${FULL_VERSION})\"}" \
-  -H "Content-Type: application/json" \
-  $DISCORD_WEBHOOK
