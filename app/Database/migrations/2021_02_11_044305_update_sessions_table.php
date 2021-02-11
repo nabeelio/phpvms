447d02bd4fe571c7602a9271c089697a86351440
Nabeel S <nabeelio@users.noreply.github.com>
2021-02-11 08:50:49 -0500

    Update the sessions table (#1030)
    * Update the sessions table
    
    * Add index on user_id

---
+++ app/Database/migrations/2021_02_11_044305_update_sessions_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Contracts\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Bring the sessions table in line with the latest
+ */
+class UpdateSessionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->string('id')->change()->primary();
+            $table->index('user_id');
+            $table->index('last_activity');
+        });
+    }
+}
