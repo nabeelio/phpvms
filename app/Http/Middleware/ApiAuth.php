97fc1dd43dffbf05976aaf0bdc37695f81c2d5f5
nabeelio <nabeel@nabeel.sh>
2021-03-08 11:19:31 -0500

    Don't block API logins #1066

--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -39,10 +39,6 @@ class ApiAuth implements Middleware
             return $this->unauthorized('User not found with key "'.$api_key.'"');
         }
 
-        if ($user->state !== UserState::ACTIVE) {
-            return $this->unauthorized('User is not ACTIVE, please contact an administrator');
-        }
-
         // Set the user to the request
         Auth::setUser($user);
         $request->merge(['user' => $user]);
