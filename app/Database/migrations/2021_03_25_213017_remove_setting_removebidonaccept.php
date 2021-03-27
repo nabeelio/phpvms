256a03b4c7e120384cb71d7a38ffb8c35355b0bc
Andreas Palm <ap@ewsp.de>
2021-03-27 13:14:19 +0100

    fix migration naming and remove obsolete code

--- app/Database/migrations/2021_03_25_213017_remove_setting_removebidonaccept.php
+++ app/Database/migrations/2021_03_25_213017_remove_setting_removebidonaccept.php
@@ -1,21 +1,10 @@
 <?php
 
-use App\Services\Installer\SeederService;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class RemoveSettingRemovebidonaccept extends Migration
+class RemoveSettingRemoveBidOnAccept extends Migration
 {
-    /**
-     * @var SeederService $seederSvc
-     */
-    private $seederSvc;
-
-    public function __construct()
-    {
-        $this->seederSvc = app(SeederService::class);
-    }
-
     /**
      * Run the migrations.
      *
@@ -26,15 +15,5 @@ class RemoveSettingRemovebidonaccept extends Migration
         DB::table('settings')
             ->where(['key' => 'pireps.remove_bid_on_accept'])
             ->delete();
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        $this->seederSvc->syncAllSettings();
     }
 }
