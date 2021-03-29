3014830a1976a41561dc78ec2ef99c1009655705
Nabeel Shahzad <nabeel@nabeel.sh>
2021-03-29 15:19:17 -0400

    Save timezone to the airport when importing

---
+++ tests/AirportTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests;
+
+use App\Models\Airport;
+
+/**
+ * Test the parsing/support class of the metar
+ */
+class AirportTest extends TestCase
+{
+    public function testSavingAirportFromApiResponse()
+    {
+        // This is the response from the API
+        $airportResponse = [
+            'icao'    => 'KJFK',
+            'iata'    => 'JFK',
+            'name'    => 'John F Kennedy International Airport',
+            'city'    => 'New York',
+            'country' => 'United States',
+            'tz'      => 'America/New_York',
+            'lat'     => 40.63980103,
+            'lon'     => -73.77890015,
+        ];
+
+        $airport = new Airport($airportResponse);
+        $this->assertEquals($airportResponse['icao'], $airport->icao);
+        $this->assertEquals($airportResponse['tz'], $airport->timezone);
+    }
+}
