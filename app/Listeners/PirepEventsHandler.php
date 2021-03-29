b418958a0de76894f97374189d4c3b4a23a340f5
Nabeel Shahzad <nabeel@nabeel.sh>
2021-03-29 14:45:13 -0400

    Add empty handler for testing

---
+++ app/Listeners/PirepEventsHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Contracts\Listener;
+use App\Events\PirepPrefiled;
+
+/**
+ * Look for and run any of the award classes. Don't modify this.
+ * See the documentation on creating awards:
+ *
+ * @url http://docs.phpvms.net/customizing/awards
+ */
+class PirepEventsHandler extends Listener
+{
+    /** The events and the callback */
+    public static $callbacks = [
+        PirepPrefiled::class    => 'onPirepPrefile',
+    ];
+
+    /**
+     * Called when a PIREP is prefiled
+     *
+     * @param PirepPrefiled $event
+     */
+    public function onPirepPrefile(PirepPrefiled $event)
+    {
+
+    }
+}
