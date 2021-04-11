68ed2355adcb2bf7542c2ff8c731d8d68f3ad6a7
Nabeel Shahzad <nshahzad@live.com>
2021-04-11 18:42:51 -0400

    Add GRAVATAR_DEFAULT_AVATAR to config

--- config/gravatar.php
+++ config/gravatar.php
@@ -5,5 +5,5 @@
 
 return [
     'url'     => 'https://www.gravatar.com/avatar/',
-    'default' => 'https://en.gravatar.com/userimage/12856995/aa6c0527a723abfd5fb9e246f0ff8af4.png',
+    'default' => env('GRAVATAR_DEFAULT_AVATAR', 'https://en.gravatar.com/userimage/12856995/aa6c0527a723abfd5fb9e246f0ff8af4.png'),
 ];
