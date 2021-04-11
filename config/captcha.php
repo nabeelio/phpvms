263d307f7ed8688a60bbefcba932890d76adf107
B.Fatih KOZ <74361521+FatihKoz@users.noreply.github.com>
2021-04-11 13:14:23 +0300

    Read Captcha Settings From ENV

--- config/captcha.php
+++ config/captcha.php
@@ -7,9 +7,9 @@
  * Don't edit this file directly, add the section to your config.php
  */
 return [
-    'enabled' => false,
-    'sitekey' => '',
-    'secret'  => '',
+    'enabled' => env('CAPTCHA_ENABLED', 'false'),
+    'sitekey' => env('CAPTCHA_SITEKEY', ''),
+    'secret'  => env('CAPTCHA_SECRET', ''),
 
     // Attributes can be found here:
     // https://developers.google.com/recaptcha/docs/display#render_param
