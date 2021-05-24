f62e7b6645ada5d0ba443cb561551eb59c85bfc9
Nabeel Shahzad <nabeel@nabeel.sh>
2021-05-24 10:54:20 -0400

    Add --force flag to migrations

--- app/Services/ModuleService.php
+++ app/Services/ModuleService.php
@@ -126,7 +126,7 @@ class ModuleService extends Service
                 'enabled' => 1,
             ]);
 
-            Artisan::call('module:migrate '.$module_name);
+            Artisan::call('module:migrate '.$module_name, ['--force' => true]);
 
             return true;
         }
@@ -225,7 +225,7 @@ class ModuleService extends Service
         }
 
         Artisan::call('config:cache');
-        Artisan::call('module:migrate '.$module);
+        Artisan::call('module:migrate '.$module, ['--force' => true]);
 
         return flash()->success('Module Installed');
     }
@@ -246,7 +246,7 @@ class ModuleService extends Service
         ]);
 
         if ($status === true) {
-            Artisan::call('module:migrate '.$module->name);
+            Artisan::call('module:migrate '.$module->name, ['--force' => true]);
         }
 
         return true;
