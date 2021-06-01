2e630a0d96ae10b10ad6ca08d370ba9d509ab4ca
Russell West <russellwwest@gmail.com>
2021-06-01 15:43:00 +0100

    aligned array fat arrow

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -40,7 +40,7 @@ class EventServiceProvider extends ServiceProvider
 
         UpdateAvailable::class => [],
         UpdateSucceeded::class => [],
-        ProfileUpdated::class => [],
+        ProfileUpdated::class  => [],
         
         // Log messages out to the console if running there
         'Illuminate\Log\Events\MessageLogged' => [
