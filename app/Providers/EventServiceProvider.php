d7d3f8cf8923797851ea15063906643eddc3de78
Russell West <russellwwest@gmail.com>
2021-06-01 15:36:19 +0100

    fix spacing issue

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -47,7 +47,6 @@ class EventServiceProvider extends ServiceProvider
         'Illuminate\Log\Events\MessageLogged' => [
             'App\Listeners\MessageLoggedListener',
         ],
-
     ];
 
     protected $subscribe = [
