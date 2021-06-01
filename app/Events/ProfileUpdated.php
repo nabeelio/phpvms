aa15cc29556f1fc3efe66326b31e98708f637342
Russell West <russellwwest@gmail.com>
2021-06-01 09:36:05 +0100

    fix style ci issues

--- app/Events/ProfileUpdated.php
+++ app/Events/ProfileUpdated.php
@@ -3,17 +3,15 @@
 namespace App\Events;
 
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class ProfileUpdated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     public $user;
     public $avatarUpdated;
