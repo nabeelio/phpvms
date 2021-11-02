00c965e1ce1638f6656573c2d768be896a8ac446
B.Fatih KOZ <fatih.koz@gmail.com>
2021-11-02 15:56:33 +0300

    Update Pirep.php

--- app/Models/Pirep.php
+++ app/Models/Pirep.php
@@ -220,7 +220,7 @@ class Pirep extends Model
     public function getIdentAttribute(): string
     {
         $flight_id = $this->airline->code;
-        $flight_id = $this->flight_number;
+        $flight_id .= $this->flight_number;
 
         if (filled($this->route_code)) {
             $flight_id .= '/C.'.$this->route_code;
