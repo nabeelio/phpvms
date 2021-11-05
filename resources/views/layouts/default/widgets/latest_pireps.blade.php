bc36ce3525bb62e39d1ea69c281c2f7345720165
B.Fatih KOZ <fatih.koz@gmail.com>
2021-11-05 12:30:48 +0300

    Update latest_pireps.blade.php

--- resources/views/layouts/default/widgets/latest_pireps.blade.php
+++ resources/views/layouts/default/widgets/latest_pireps.blade.php
@@ -2,15 +2,15 @@
   @foreach($pireps as $p)
     <tr>
       <td style="padding-right: 10px;">
-        <span class="title">{{ $p->airline->code }} {{ $p->flight_number }}</span>
+        <span class="title">{{ $p->ident }}</span>
       </td>
       <td>
         <a href="{{route('frontend.airports.show', [$p->dpt_airport_id])}}">{{$p->dpt_airport_id}}</a>
         &nbsp;-&nbsp;
-        <a href="{{route('frontend.airports.show', [$p->arr_airport_id])}}">{{$p->arr_airport_id}}</a>&nbsp;
-        @if(!empty($p->aircraft))
-          {{ optional($p->aircraft)->registration }} ({{ $p->aircraft->icao }})
-        @endif
+        <a href="{{route('frontend.airports.show', [$p->arr_airport_id])}}">{{$p->arr_airport_id}}</a>
+      </td>
+      <td>
+        {{ optional($p->aircraft)->ident }}
       </td>
     </tr>
   @endforeach
