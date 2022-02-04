825aaf1a35dda18a430c094eeefb1ebb44168289
loko06320 <montet.steve@gmail.com>
2022-02-04 22:57:47 +0100

    Add files via upload

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -2,7 +2,7 @@
 
 return [
     'failed'                   => 'Les informations identification ne correspondent pas.',
-    'throttle'                 => 'Trop de tentatives de connexion. Veuillez réessayer dans quelque minutes.',
+    'throttle'                 => 'Trop de tentatives de connexion. Veuillez réessayer dans quelques minutes.',
     'password'                 => 'Mot de Passe',
     'createaccount'            => 'Créer un Compte',
     'forgotpassword'           => 'Mot de Passe Oublié',
@@ -15,10 +15,10 @@ return [
     'pendingmessage'           => 'Votre enregistrement et en attente. Veuillez vérifier vos emails!',
     'registrationsubmitted'    => 'Votre Enregistrement à été transmis',
     'registrationconfirmation' => 'Confirmation inscription',
-    'confirmationmessage'      => 'Votre demande a été transmise. Elle nécessite une approbation par un administrateur. Apès examun de votre demande, vous recevrez un courriel de confirmation.',
+    'confirmationmessage'      => 'Votre demande a été transmise. Elle nécessite une approbation par un administrateur. Apès examin de votre demande, vous recevrez un courriel de confirmation.',
     'registrationdenied'       => 'Inscription rejetée',
     'deniedmessage'            => 'votre inscription à été rejetée.Veuillez contacter un administrateur.',
-    'accountsuspended'         => 'Accompte Suspendu',
+    'accountsuspended'         => 'Compte Suspendu',
     'suspendedmessage'         => 'Votre compte a été suspendu. Veuillez contacter un administrateur.',
     'transferhours'            => 'Heures de Transfert',
 ];
