05028f121fd1e0fc6fca61d384fa9419364d340d
B.Fatih KOZ <fatih.koz@gmail.com>
2022-02-13 03:30:18 +0300

    StyleFix

--- app/Notifications/NotificationEventsHandler.php
+++ app/Notifications/NotificationEventsHandler.php
@@ -175,7 +175,7 @@ class NotificationEventsHandler extends Listener
     }
 
     /**
-     * Status Change notification. 
+     * Status Change notification.
      * Reduced the messages (Boarding, Pushback, TakeOff, Landing and non-normals only)
      * If needed array can be tied to a setting at admin side for further customization
      */
