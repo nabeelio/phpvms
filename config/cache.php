a7490337ea8ef6d9f7278b93e110696bfda2384e
Arthur Parienté <arthur.pariente78960@gmail.com>
2023-01-11 18:12:51 +0100

    Store modules queries when env is prod

--- config/cache.php
+++ config/cache.php
@@ -21,6 +21,10 @@ return [
             'key'  => 'settings.', //append setting key
             'time' => 60 * 60 * 24, // Cache for 1 day
         ],
+        'MODULES' => [
+            'key'  => 'modules',
+            'time' => 60 * 60 * 24, // Cache for 1 day
+        ],
         'TAF_WEATHER_LOOKUP' => [
             'key'  => 'airports.weather.taf.', // append icao
             'time' => 60 * 60,  // Cache for 60 minutes
