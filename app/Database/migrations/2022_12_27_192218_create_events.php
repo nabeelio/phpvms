95282d2eee981415eef942a7874653589f79b200
B.Fatih KOZ <fatih.koz@gmail.com>
2023-02-06 20:47:14 +0300

    Update 2022_12_27_192218_create_events.php
    use `primary` instead of `increments` as requested

--- app/Database/migrations/2022_12_27_192218_create_events.php
+++ app/Database/migrations/2022_12_27_192218_create_events.php
@@ -10,7 +10,7 @@ return new class() extends Migration {
     {
         // Create events table
         Schema::create('events', function (Blueprint $table) {
-            $table->increments('id');
+            $table->primary('id');
             $table->unsignedInteger('type')->default(0);
             $table->string('name', 250);
             $table->text('description')->nullable();
