c7c499e8798e4f666df5a263d5b1d20f9c839bab
B.Fatih KOZ <fatih.koz@gmail.com>
2023-07-26 17:19:47 +0300

    add description field (#1559)
    Co-authored-by: Nabeel S <nabeelio@users.noreply.github.com>

--- resources/views/admin/pirepfields/fields.blade.php
+++ resources/views/admin/pirepfields/fields.blade.php
@@ -1,19 +1,21 @@
 <div class="row">
-  <div class="form-group col-sm-6">
+  <div class="form-group col-sm-5">
     {{ Form::label('name', 'Name:') }}&nbsp;&nbsp;<span class="required">*</span>
     {{ Form::text('name', null, ['class' => 'form-control']) }}
     <p class="text-danger">{{ $errors->first('name') }}</p>
   </div>
-
-  <!-- Required Field -->
-  <div class="form-group col-sm-6">
+  <div class="form-group col-sm-5">
+    {{ Form::label('description', 'Desc:') }}
+    {{ Form::text('description', null, ['class' => 'form-control']) }}
+    <p class="text-danger">{{ $errors->first('description') }}</p>
+  </div>
+  <div class="form-group col-sm-2">
     {{ Form::label('required', 'Required:') }}
     {{ Form::hidden('required', 0) }}
     {{ Form::checkbox('required', null) }}
   </div>
 </div>
 <div class="row">
-  <!-- Submit Field -->
   <div class="form-group col-sm-12">
     {{ Form::button('Save', ['type' => 'submit', 'class' => 'btn btn-success']) }}
     <a href="{{ route('admin.pirepfields.index') }}" class="btn btn-default">Cancel</a>
--- resources/views/admin/pirepfields/table.blade.php
+++ resources/views/admin/pirepfields/table.blade.php
@@ -1,39 +1,34 @@
 <div class="content table-responsive table-full-width">
-
   <div class="header">
     @component('admin.components.info')
       PIREP fields are only shown for manual PIREPs.
     @endcomponent
   </div>
-
   <table class="table table-hover table-responsive" id="pirepFields-table">
     <thead>
-    <th>Name</th>
-    <th style="text-align: center;">Required</th>
-    <th></th>
+      <th>Name</th>
+      <th>Description</th>
+      <th style="text-align: center;">Required</th>
+      <th></th>
     </thead>
     <tbody>
-    @foreach($fields as $field)
-      <tr>
-        <td>{{ $field->name }}</td>
-        <td style="text-align: center;">
-          @if($field->required === true)
-            <span class="label label-success">Required</span>
-          @endif
-        </td>
-        <td class="text-right">
-          {{ Form::open(['route' => ['admin.pirepfields.destroy', $field->id], 'method' => 'delete']) }}
-          <a href="{{ route('admin.pirepfields.edit', [$field->id]) }}"
-             class='btn btn-sm btn-success btn-icon'>
-            <i class="fas fa-pencil-alt"></i></a>
-
-          {{ Form::button('<i class="fa fa-times"></i>',
-                       ['type' => 'submit', 'class' => 'btn btn-sm btn-danger btn-icon',
-                        'onclick' => "return confirm('Are you sure?')"]) }}
-          {{ Form::close() }}
-        </td>
-      </tr>
-    @endforeach
+      @foreach($fields as $field)
+        <tr>
+          <td>{{ $field->name }}</td>
+          <td>{{ $field->description }}</td>
+          <td style="text-align: center;">
+            @if($field->required === true)
+              <span class="label label-success">Required</span>
+            @endif
+          </td>
+          <td class="text-right">
+            {{ Form::open(['route' => ['admin.pirepfields.destroy', $field->id], 'method' => 'delete']) }}
+            <a href="{{ route('admin.pirepfields.edit', [$field->id]) }}" class='btn btn-sm btn-success btn-icon'><i class="fas fa-pencil-alt"></i></a>
+            {{ Form::button('<i class="fa fa-times"></i>', [ 'type' => 'submit', 'class' => 'btn btn-sm btn-danger btn-icon', 'onclick' => "return confirm('Are you sure?')"]) }}
+            {{ Form::close() }}
+          </td>
+        </tr>
+      @endforeach
     </tbody>
   </table>
 </div>
