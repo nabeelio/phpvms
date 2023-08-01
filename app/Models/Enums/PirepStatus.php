e3bd97a1733490efcccb96873e68dd008e5e639d
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2023-08-01 15:12:04 -0500

    Style changes

--- app/Models/Enums/PirepStatus.php
+++ app/Models/Enums/PirepStatus.php
@@ -11,31 +11,31 @@ use App\Contracts\Enum;
  */
 class PirepStatus extends Enum
 {
-    public const INITIATED     = 'INI';
-    public const SCHEDULED     = 'SCH';
-    public const BOARDING      = 'BST';
-    public const RDY_START     = 'RDT';
-    public const PUSHBACK_TOW  = 'PBT';
-    public const DEPARTED      = 'OFB'; // Off block
-    public const RDY_DEICE     = 'DIR';
-    public const STRT_DEICE    = 'DIC';
-    public const GRND_RTRN     = 'GRT'; // Ground return
-    public const TAXI          = 'TXI'; // Taxi
-    public const TAKEOFF       = 'TOF';
-    public const INIT_CLIM     = 'ICL';
-    public const AIRBORNE      = 'TKO';
-    public const ENROUTE       = 'ENR';
-    public const DIVERTED      = 'DV';
-    public const APPROACH      = 'TEN';
+    public const INITIATED = 'INI';
+    public const SCHEDULED = 'SCH';
+    public const BOARDING = 'BST';
+    public const RDY_START = 'RDT';
+    public const PUSHBACK_TOW = 'PBT';
+    public const DEPARTED = 'OFB'; // Off block
+    public const RDY_DEICE = 'DIR';
+    public const STRT_DEICE = 'DIC';
+    public const GRND_RTRN = 'GRT'; // Ground return
+    public const TAXI = 'TXI'; // Taxi
+    public const TAKEOFF = 'TOF';
+    public const INIT_CLIM = 'ICL';
+    public const AIRBORNE = 'TKO';
+    public const ENROUTE = 'ENR';
+    public const DIVERTED = 'DV';
+    public const APPROACH = 'TEN';
     public const APPROACH_ICAO = 'APR';
-    public const ON_FINAL      = 'FIN';
-    public const LANDING       = 'LDG';
-    public const LANDED        = 'LAN';
-    public const ON_BLOCK      = 'ONB';
-    public const ARRIVED       = 'ARR';
-    public const CANCELLED     = 'DX';
+    public const ON_FINAL = 'FIN';
+    public const LANDING = 'LDG';
+    public const LANDED = 'LAN';
+    public const ON_BLOCK = 'ONB';
+    public const ARRIVED = 'ARR';
+    public const CANCELLED = 'DX';
     public const EMERG_DESCENT = 'EMG';
-    public const PAUSED        = 'PSD';
+    public const PAUSED = 'PSD';
 
     protected static array $labels = [
         self::INITIATED     => 'pireps.status.initialized',
