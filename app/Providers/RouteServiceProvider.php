170135b1026a7e45b688c594725b6e51cfeb4243
ArthurHetem <arthurhetemgames@gmail.com>
2023-08-14 18:39:39 -0300

    Change setting toggle to config/phpvms.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -110,7 +110,7 @@ class RouteServiceProvider extends ServiceProvider
                 'namespace'  => 'Frontend',
                 'prefix'     => '',
                 'as'         => 'frontend.',
-                'middleware' => (setting('security.require_email_verification', false) ? ['auth', 'verified'] : ['auth']),
+                'middleware' => (config('phpvms.registration.email_verification', false) ? ['auth', 'verified'] : ['auth']),
             ], function () {
                 Route::resource('dashboard', 'DashboardController');
 
