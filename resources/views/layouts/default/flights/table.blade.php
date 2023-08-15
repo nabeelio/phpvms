83b62629c80e93850af7bd2f9dad9881953815ce
Arthur Parienté <arthur.pariente78960@gmail.com>
2023-08-15 14:47:34 +0200

    Add booked aircraft to simbrief generate url

--- resources/views/layouts/default/flights/table.blade.php
+++ resources/views/layouts/default/flights/table.blade.php
@@ -90,7 +90,10 @@
             @else
               <!-- Show button if the bids-only is disable, or if bids-only is enabled, they've saved it -->
               @if ($simbrief_bids === false || ($simbrief_bids === true && isset($saved[$flight->id])))
-                <a href="{{ route('frontend.simbrief.generate') }}?flight_id={{ $flight->id }}"
+                @php
+                  $aircraft_id = App\Models\Bid::find($saved[$flight->id])->aircraft_id;
+                @endphp
+                <a href="{{ route('frontend.simbrief.generate') }}?flight_id={{ $flight->id }}@if($aircraft_id)&aircraft_id={{ $aircraft_id }} @endif"
                    class="btn btn-sm btn-outline-primary">
                   Create Simbrief Flight Plan
                 </a>
