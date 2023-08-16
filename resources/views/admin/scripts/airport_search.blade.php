3066bd16f20656fdd7990ba2460231c9ce083d8d
Arthur Hetem <arthurhetemgames@gmail.com>
2023-08-16 09:55:51 -0300

    Fix typo in admin search script

--- resources/views/admin/scripts/airport_search.blade.php
+++ resources/views/admin/scripts/airport_search.blade.php
@@ -4,7 +4,7 @@ $(document).ready(function () {
     ajax: {
       url: '{{ Config::get("app.url") }}/api/airports/search',
       data: function (params) {
-        const hub_only = $(this).hasClass('hubs_only') ? 1 : 0;
+        const hub_only = $(this).hasClass('hub_only') ? 1 : 0;
         return {
           search: params.term,
           hub: hub_only,
