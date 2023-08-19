75bf37ba1d899bad8eb4805d9e8255b0fc75488f
B.Fatih KOZ <fatih.koz@gmail.com>
2023-08-19 03:27:36 +0300

    Remove BelongsTo
    as it is not being used

--- app/Models/Airline.php
+++ app/Models/Airline.php
@@ -8,7 +8,6 @@ use App\Models\Traits\FilesTrait;
 use App\Models\Traits\JournalTrait;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
