58dcb451998d6cf92abe8ca98adde7f257863332
Toni Vicente <arv187@users.noreply.github.com>
2023-08-23 15:54:12 +0200

    Send comments in rejected email (#1593)
    The pilot not only receives a comment saying that the pirep has been rejected, but also includes the comments made in the pirep.
    Add it if you think it is useful to include it in the core for all phpvms7 users.
    
    Co-authored-by: Nabeel S <nabeelio@users.noreply.github.com>

--- resources/views/notifications/mail/pirep/rejected.blade.php
+++ resources/views/notifications/mail/pirep/rejected.blade.php
@@ -2,7 +2,12 @@
   # PIREP Rejected!
 
   Your PIREP has been rejected
-
+  @if($pirep->comments->count() > 0)
+    ## Comments
+    @foreach($pirep->comments as $comment)
+      - {{ $comment->comment }}
+    @endforeach
+  @endif 
   @component('mail::button', ['url' => route('frontend.pireps.show', [$pirep->id])])
     View PIREP
   @endcomponent
