f95c732ef7c3b70949089be7a923a973c8ba1478
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2023-10-13 10:35:55 -0500

    Style fixes

--- app/Database/migrations/2023_05_08_174436_add_fare_details_to_pirep.php
+++ app/Database/migrations/2023_05_08_174436_add_fare_details_to_pirep.php
@@ -26,7 +26,6 @@ return new class() extends Migration {
                     ->nullable()
                     ->after('capacity');
                 $table->softDeletes();
-
             });
         }
 
