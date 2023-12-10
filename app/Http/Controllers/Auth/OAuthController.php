5ef5e122cce28ca7f79694523bf7ae0dd0f99684
Arthur Pariente <arthur.pariente78960@gmail.com>
2023-12-10 17:19:48 +0100

    Retrieve the discord_private_channel_id in OAuthController

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Contracts\Controller;
+use GuzzleHttp\Client;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
+use function PHPUnit\Framework\isNull;
 
 class OAuthController extends Controller
 {
@@ -19,8 +22,30 @@ class OAuthController extends Controller
         $user = Socialite::driver('discord')->user();
 
         if ($user->getId()) {
+
+            // Let's retrieve the private_channel_id
+            if (!is_null(env('DISCORD_BOT_TOKEN'))) {
+                try {
+                    $httpClient = new Client();
+                    $response = $httpClient->request('POST', 'https://discord.com/api/users/@me/channels', [
+                        'headers' => [
+                            'Authorization' => 'Bot ' . env('DISCORD_BOT_TOKEN')
+                        ],
+                        'json' => [
+                            'recipient_id' => $user->getId(),
+                        ],
+                    ]);
+
+                    $privateChannel = json_decode($response->getBody()->getContents(), true)['id'];
+                } catch (\Exception $e) {
+                    Log::error('Discord OAuth Error: ' .$e->getMessage());
+                    $privateChannel = null;
+                }
+            }
+
             Auth::user()?->update([
-                'discord_id' => $user->getId(),
+                'discord_id'                 => $user->getId(),
+                'discord_private_channel_id' => $privateChannel ?? null,
             ]);
 
             flash()->success('Discord account linked!');
@@ -34,7 +59,8 @@ class OAuthController extends Controller
     public function logoutDiscordProvider(): RedirectResponse
     {
         Auth::user()?->update([
-            'discord_id' => null,
+            'discord_id'                 => null,
+            'discord_private_channel_id' => null,
         ]);
 
         flash()->success('Discord account unlinked!');
