c981cbd57788514e29300c63ea9fd5439d636179
B.Fatih KOZ <fatih.koz@gmail.com>
2023-12-30 14:22:06 +0300

    Another StlyCI Fix

--- app/Notifications/Messages/NewsAdded.php
+++ app/Notifications/Messages/NewsAdded.php
@@ -5,7 +5,6 @@ namespace App\Notifications\Messages;
 use App\Contracts\Notification;
 use App\Models\News;
 use App\Notifications\Channels\Discord\DiscordMessage;
-use App\Notifications\Channels\Discord\DiscordWebhook;
 use App\Notifications\Channels\MailChannel;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
