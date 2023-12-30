7ae9515269a7517a0d580fb2683088c97f4533d9
B.Fatih KOZ <fatih.koz@gmail.com>
2023-12-30 23:53:50 +0300

    Remove discord stuff

--- app/Notifications/Messages/NewsAdded.php
+++ app/Notifications/Messages/NewsAdded.php
@@ -4,7 +4,6 @@ namespace App\Notifications\Messages;
 
 use App\Contracts\Notification;
 use App\Models\News;
-use App\Notifications\Channels\Discord\DiscordMessage;
 use App\Notifications\Channels\MailChannel;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -30,38 +29,6 @@ class NewsAdded extends Notification implements ShouldQueue
     {
         return ['mail'];
     }
-
-    /**
-     * @param News  $news
-     * @param mixed $notifiable
-     *
-     * @return DiscordMessage|null
-     */
-
-    /*
-     * DASHED OUT ON PURPOSE TO FIX ISSUE  #1741
-     * AS IT GENERATES EXCEPTIONS DUE TO MISSING USER OBJECT
-     * AND GENERATES MULTIPLE MESSAGES IN DISCORD
-     *
-        public function toDiscordChannel($news): ?DiscordMessage
-        {
-            if (empty(setting('notifications.discord_public_webhook_url'))) {
-                return null;
-            }
-
-            $dm = new DiscordMessage();
-            return $dm->webhook(setting('notifications.discord_public_webhook_url'))
-                ->success()
-                ->title('News: '.$news->subject)
-                ->author([
-                    'name'     => $news->user->ident.' - '.$news->user->name_private,
-                    'url'      => '',
-                    'icon_url' => $news->user->resolveAvatarUrl(),
-                ])
-                ->description($news->body);
-        }
-    *
-    */
 
     /**
      * Get the array representation of the notification.
