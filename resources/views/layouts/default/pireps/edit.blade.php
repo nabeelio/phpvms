d0c24ff9383c218cf28b6908fbb8d25d1edbf725
Arthur Pariente <arthur.pariente78960@gmail.com>
2024-01-19 18:04:33 +0100

    Fix typo

--- resources/views/layouts/default/pireps/edit.blade.php
+++ resources/views/layouts/default/pireps/edit.blade.php
@@ -4,7 +4,7 @@
   <div class="row">
     <div class="col-md-12">
       <h2>@lang('pireps.editflightreport')</h2>
-      @include('flash::message')$
+      @include('flash::message')
       <form method="post" action="{{ route('frontend.pireps.update', $pirep->id) }}" class="form-group">
         @method('PATCH')
         @csrf
