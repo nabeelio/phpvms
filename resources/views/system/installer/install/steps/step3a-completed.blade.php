308aa4a7752ad7cb8731cef8721b7da08483b937
Arthur Hetem <arthurhetemgames@gmail.com>
2024-05-17 15:27:48 -0300

    Fix wrong button types and env file

--- resources/views/system/installer/install/steps/step3a-completed.blade.php
+++ resources/views/system/installer/install/steps/step3a-completed.blade.php
@@ -2,18 +2,18 @@
 @section('title', 'Installation Completed!')
 
 @section('content')
-  <div style="align-content: center;">
-    <form method="get" action="{{ route('installer.complete') }}">
-      @csrf
+    <div style="align-content: center;">
+        <form method="get" action="{{ route('installer.complete') }}">
+            @csrf
 
-    <h4>Installer Completed!</h4>
+            <h4>Installer Completed!</h4>
 
-    <p>Edit the <span class="code">env.php</span> to fill in some additional settings.</p>
-    <p>Click the button to proceed to the login screen!</p>
+            <p>Edit the <span class="code">.env</span> to fill in some additional settings.</p>
+            <p>Click the button to proceed to the login screen!</p>
 
-    <p style="text-align: right">
-      <button type="submit" class="btn btn-success">Continue to Log-In >></button>
-    </p>
-    </form>
-  </div>
+            <p style="text-align: right">
+                <button type="submit" class="btn btn-success">Continue to Log-In >></button>
+            </p>
+        </form>
+    </div>
 @endsection
