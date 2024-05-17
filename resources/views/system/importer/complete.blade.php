1eb4d8c98e178c6b1f836b9b8c069a6f952d9e59
Arthur Hetem <arthurhetemgames@gmail.com>
2024-05-17 10:18:44 -0300

    Fix wrong action on importer complete form

--- resources/views/system/importer/complete.blade.php
+++ resources/views/system/importer/complete.blade.php
@@ -2,17 +2,17 @@
 @section('title', 'Import Completed!')
 
 @section('content')
-  <div style="align-content: center;">
-    <form method="post" action="{{ route('importer.complete') }}">
-      @csrf
-    <h4>Installer Completed!</h4>
+    <div style="align-content: center;">
+        <form method="get" action="{{ route('importer.complete') }}">
+            @csrf
+            <h4>Installer Completed!</h4>
 
-    <p>Edit the <span class="code">config.php</span> to fill in some additional settings. </p>
-    <p>Click the button to proceed to the login screen!</p>
+            <p>Edit the <span class="code">config.php</span> to fill in some additional settings. </p>
+            <p>Click the button to proceed to the login screen!</p>
 
-    <p style="text-align: right">
-      <button type="submit" class="btn btn-success">Import Complete! Continue to Log-In >></button>
-    </p>
-    </form>
-  </div>
+            <p style="text-align: right">
+                <button type="submit" class="btn btn-success">Import Complete! Continue to Log-In >></button>
+            </p>
+        </form>
+    </div>
 @endsection
