4e277d70dfb44b2b62daabd4a92f952949000316
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2024-07-15 17:23:28 -0500

    Fix deployment message

--- .github/scripts/discord.sh
+++ .github/scripts/discord.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "discord_msg=Version ${$NBGV_SemVer2} is available, download: [zip](http://downloads.phpvms.net/$ZIP_NAME) | [tar](http://downloads.phpvms.net/$TAR_NAME)" >> "$GITHUB_OUTPUT"
+echo "discord_msg=Version ${NBGV_SemVer2} is available, download: [zip](http://downloads.phpvms.net/$ZIP_NAME) | [tar](http://downloads.phpvms.net/$TAR_NAME)" >> "$GITHUB_OUTPUT"
