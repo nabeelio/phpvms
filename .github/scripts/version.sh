fcd4a5709a2d85f2ad1414b3c7ad109232d2b23b
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2024-07-15 16:50:08 -0500

    Use Nerdbank.GitVersioning instead

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 if test "$GIT_TAG_NAME"; then
-  export VERSION=$fullSemVer
+  export VERSION=$NBGV_SemVer2
 
   # Pass in the tag as the version to write out
-  php artisan phpvms:version --write --write-full-version "${fullSemVer}"
-  export FULL_VERSION=$fullSemVer
+  php artisan phpvms:version --write --write-full-version "${NBGV_SemVer2}"
+  export FULL_VERSION=$NBGV_SemVer2
 else
   export BRANCH=${GITHUB_REF##*/}
   echo "On branch $BRANCH"
 
   # This now includes the pre-release version, so "-dev" by default
-  export VERSION=$fullSemVer
+  export VERSION=$NBGV_SemVer2
 
   # Don't pass in a version here, just write out the latest hash
-  php artisan phpvms:version --write "${fullSemVer}"
-  export FULL_VERSION=$fullSemVer
+  php artisan phpvms:version --write "${$NBGV_SemVer2}"
+  export FULL_VERSION=$NBGV_SemVer2
 fi
 
 export FILE_NAME="phpvms-${VERSION}"
@@ -29,4 +29,4 @@ echo "FILE_NAME=${FILE_NAME}" >> "$GITHUB_ENV"
 echo "TAR_NAME=${TAR_NAME}" >> "$GITHUB_ENV"
 echo "ZIP_NAME=${ZIP_NAME}" >> "$GITHUB_ENV"
 echo "BASE_DIR=${BASE_DIR}" >> "$GITHUB_ENV"
-echo "FULL_VERSION=${FULL_VERSION}" >> "$GITHUB_ENV"
+echo "FULL_VERSION=${$NBGV_SemVer2}" >> "$GITHUB_ENV"
