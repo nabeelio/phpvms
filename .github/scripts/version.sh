2ae4b076153aba41a42ce5215c97a9a241ecc5e5
Nabeel S. <nabeelio@users.noreply.github.com>
2024-07-15 17:16:34 -0500

    Use Nerdbank.GitVersioning instead (#1844)
    * Use Nerdbank.GitVersioning instead
    
    * remove old GitVersioning
    
    * Make sure version is read for deployments
    
    * Syntax error
    
    * Syntax errors fix
    
    * Syntax errors fix
    
    * Apply fixes from StyleCI

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 if test "$GIT_TAG_NAME"; then
-  export VERSION=$fullSemVer
+  export VERSION=$NBGV_SemVer2
 
-  # Pass in the tag as the version to write out
-  php artisan phpvms:version --write --write-full-version "${fullSemVer}"
-  export FULL_VERSION=$fullSemVer
+  php artisan phpvms:version --write --write-full-version "${VERSION}"
+  export FULL_VERSION=$NBGV_SemVer2
 else
   export BRANCH=${GITHUB_REF##*/}
   echo "On branch $BRANCH"
 
   # This now includes the pre-release version, so "-dev" by default
-  export VERSION=$fullSemVer
+  export VERSION=$NBGV_SemVer2
 
   # Don't pass in a version here, just write out the latest hash
-  php artisan phpvms:version --write "${fullSemVer}"
-  export FULL_VERSION=$fullSemVer
+  php artisan phpvms:version --write "${VERSION}"
+  export FULL_VERSION=$NBGV_SemVer2
 fi
 
 export FILE_NAME="phpvms-${VERSION}"
