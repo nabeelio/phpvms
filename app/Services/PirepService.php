2f8a8dfb0e16f9a9ed242b0bd3f655f82c98a4a0
B.Fatih KOZ <fatih.koz@gmail.com>
2024-07-17 23:28:44 +0300

    Update PirepService.php (#1847)
    * Update PirepService.php
    
    * Use SimBrief details for planned distance when available
    * Do NOT update $pirep->distance with flight/simbrief
    
    * Update PirepService.php
    
    use the `$flight->distance` as there is no attribute called `$flight->planned_distance`
    
    * Update PirepService.php
    
    Use `air_distance` as it is much realistic for pirep records.

--- app/Services/PirepService.php
+++ app/Services/PirepService.php
@@ -233,9 +233,9 @@ class PirepService extends Service
 
         $pirep->status = PirepStatus::ARRIVED;
 
-        // Copy some fields over from Flight if we have it
+        // Copy some fields over from Flight/SimBrief if we have it
         if ($pirep->flight) {
-            $pirep->planned_distance = $pirep->flight->planned_distance;
+            $pirep->planned_distance = isset($pirep->flight->simbrief) ? $pirep->flight->simbrief->xml->general->air_distance : $pirep->flight->distance;
             $pirep->planned_flight_time = $pirep->flight->flight_time;
         }
 
@@ -326,9 +326,9 @@ class PirepService extends Service
             $pirep->submitted_at = Carbon::now('UTC');
         }
 
-        // Copy some fields over from Flight if we have it
+        // Copy some fields over from Flight/SimBrief if we have it
         if ($pirep->flight) {
-            $pirep->distance = $pirep->flight->distance;
+            $pirep->planned_distance = isset($pirep->flight->simbrief) ? $pirep->flight->simbrief->xml->general->air_distance : $pirep->flight->distance;
             $pirep->planned_flight_time = $pirep->flight->flight_time;
         }
 
