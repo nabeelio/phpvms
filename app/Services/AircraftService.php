e3517c6d949e737344f718177f29a0d24217ff91
Arthur Parienté <41431456+arthurpar06@users.noreply.github.com>
2024-09-11 17:02:01 +0200

    Fix aircraft's flight time calculation (#1853)
    * Fix aircraft flight time in reject pirep
    
    * Recalculate stats only with accepted pireps
    
    * Recalculate aircraft stats only for active aircraft
    
    ---------
    
    Co-authored-by: Nabeel S. <nabeelio@users.noreply.github.com>

--- app/Services/AircraftService.php
+++ app/Services/AircraftService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Contracts\Service;
 use App\Models\Aircraft;
+use App\Models\Enums\AircraftStatus;
+use App\Models\Enums\PirepState;
 use App\Models\Pirep;
 
 class AircraftService extends Service
@@ -13,9 +15,10 @@ class AircraftService extends Service
      */
     public function recalculateStats()
     {
-        $allAircraft = Aircraft::all(); // TODO: Soft delete
+        $allAircraft = Aircraft::where('status', AircraftStatus::ACTIVE)->get(); // TODO: Soft delete
         foreach ($allAircraft as $aircraft) {
             $pirep_time_total = Pirep::where('aircraft_id', $aircraft->id)
+                ->where('state', PirepState::ACCEPTED)
                 ->sum('flight_time');
             $aircraft->flight_time = $pirep_time_total;
             $aircraft->save();
